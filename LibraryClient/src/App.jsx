f4eabfa5cba57e79b306f074d68147652f5ae8f5
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-09-17 21:58:26 +0300

    imported router, pages

--- LibraryClient/src/App.jsx
+++ LibraryClient/src/App.jsx
@@ -1,5 +1,10 @@
-
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import PublisherPage from './pages/PublisherPage';
+import CategoryPage from './pages/CategoryPage';
+import BookPage from './pages/BookPage';
+import AuthorPage from './pages/AuthorPage';
+import BorrowBookPage from './pages/BorrowBookPage';
 
 function App() {
 
