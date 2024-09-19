f46cf1a4074bd92613513d6580b4d2f9be179766
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-09-19 21:19:53 +0300

    updated

--- LibraryClient/src/App.jsx
+++ LibraryClient/src/App.jsx
@@ -1,26 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import PublisherPage from './pages/PublisherPage';
-import CategoryPage from './pages/CategoryPage';
-import BookPage from './pages/BookPage';
-import AuthorPage from './pages/AuthorPage';
-import BorrowBookPage from './pages/BorrowBookPage';
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import PublisherPage from "./pages/PublisherPage";
+import CategoryPage from "./pages/CategoryPage";
+import BookPage from "./pages/BookPage";
+import AuthorPage from "./pages/AuthorPage";
+import BorrowingPage from "./pages/BorrowingPage";
+import Navbar from "./components/Navbar";
 
+import './App.css'
 function App() {
-
-
   return (
-    <Router>
+    <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/publisher" element={<PublisherPage />} />
-        <Route path="/category" element={<CategoryPage />} />
-        <Route path="/book" element={<BookPage />} />
-        <Route path="/author" element={<AuthorPage />} />
-        <Route path="/borrow" element={<BorrowBookPage />} />
+        <Route path="/publishers" element={<PublisherPage />} />
+        <Route path="/categories" element={<CategoryPage />} />
+        <Route path="/books" element={<BookPage />} />
+        <Route path="/authors" element={<AuthorPage />} />
+        <Route path="/borrows" element={<BorrowingPage />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
-export default App
+export default App;
