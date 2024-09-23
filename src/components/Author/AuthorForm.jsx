53f7d42d05d5d8e8403359f040f6b2c82dd6aa94
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-09-24 01:30:42 +0300

    button styles updated

--- src/components/Author/AuthorForm.jsx
+++ src/components/Author/AuthorForm.jsx
@@ -3,8 +3,8 @@ import { TextField, Button, Typography, Box } from "@mui/material";
 
 function AuthorForm({ author, onChange, onSubmit, error }) {
   return (
-    <Box className="form-container">
-      <Typography component="h1" variant="h5">
+    <Box className="form-container" sx={{ padding: "2rem", backgroundColor: "#f5f5f5", borderRadius: "8px" }}>
+      <Typography component="h1" variant="h5" sx={{ marginBottom: "1rem" }}>
         Add Author
       </Typography>
       <form onSubmit={onSubmit} className="author-form">
@@ -28,11 +28,19 @@ function AuthorForm({ author, onChange, onSubmit, error }) {
           value={author.country}
           onChange={(e) => onChange("country", e.target.value)}
         />
-        <Button type="submit" fullWidth variant="contained">
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{
+            backgroundColor: "#303f9f",
+            ":hover": { backgroundColor: "#ffeb3b", color: "#303f9f" },
+            marginTop: "1rem",
+          }}
+        >
           Add Author
         </Button>
       </form>
-     
     </Box>
   );
 }
--- src/components/Author/AuthorTable.jsx
+++ src/components/Author/AuthorTable.jsx
@@ -9,8 +9,16 @@ import Button from "@mui/material/Button";
 
 export default function AuthorTable({ authors, onEdit, onDelete, onAssign, onAdd }) {
   return (
-    <Paper className="table-container">
-      <Button variant="contained" color="primary" onClick={onAdd} style={{ marginBottom: '16px' }}>
+    <Paper className="table-container" sx={{ padding: "2rem", backgroundColor: "#f5f5f5", borderRadius: "8px" }}>
+      <Button
+        variant="contained"
+        onClick={onAdd}
+        sx={{
+          backgroundColor: "#303f9f",
+          ":hover": { backgroundColor: "#ffeb3b", color: "#303f9f" },
+          marginBottom: "16px",
+        }}
+      >
         Add New Author
       </Button>
       <Table>
@@ -26,23 +34,36 @@ export default function AuthorTable({ authors, onEdit, onDelete, onAssign, onAdd
         <TableBody>
           {authors.map((author) => (
             <TableRow key={author.id}>
-              <TableCell>{author.id}</TableCell> 
+              <TableCell>{author.id}</TableCell>
               <TableCell component="th" scope="row">
                 {author.name}
               </TableCell>
               <TableCell align="center">{author.birthDate}</TableCell>
               <TableCell align="center">{author.country}</TableCell>
               <TableCell align="center">
-              
-                <Button variant="contained"
-                    color="primary" onClick={() => onEdit(author)} style={{ marginRight: '10px' ,width:'2px' }}>
+                <Button
+                  variant="contained"
+                  sx={{
+                    backgroundColor: "#303f9f",
+                    ":hover": { backgroundColor: "#ffeb3b", color: "#303f9f" },
+                    marginRight: "10px",
+                  }}
+                  onClick={() => onEdit(author)}
+                >
                   Edit
                 </Button>
-                <Button variant="outlined"
-                    color="error" onClick={() => onDelete(author.id)} style={{ marginRight: '10px',width:'4px'  }}>
+                <Button
+                  variant="outlined"
+                  color="error"
+                  sx={{
+
+                    ":hover": { backgroundColor: "#ffeb3b", color: "#303f9f" },
+                    marginRight: "10px",
+                  }}
+                  onClick={() => onDelete(author.id)}
+                >
                   Delete
                 </Button>
-                
               </TableCell>
             </TableRow>
           ))}
